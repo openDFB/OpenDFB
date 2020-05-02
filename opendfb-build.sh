4638de1ce77343208a98fe295a917cda04196b0c
kin <kingjinkin@gmail.com>
2020-05-02 07:47:53 -0700

    remove some build file

--- opendfb-build.sh
+++ opendfb-build.sh
@@ -1,2 +1,2 @@
-./autogen.sh --enable-multi --enable-sawman   --enable-divine  --disable-one --enable-network  --disable-fusionsound --disable-fusiondale --disable-video4linux  --disable-network --disable-pnm  --with-gfxdrivers=none   --with-inputdrivers=keyboard,linuxinput,lirc,ps2mouse,serialmouse,lirc   --prefix=/opt/opendfb  --with-tests  #--enable-debug;
-make  -j3;sudo make install
+./autogen.sh --enable-multi --enable-sawman   --enable-divine --enable-network --disable-network --disable-pnm  --with-gfxdrivers=none   --with-inputdrivers=keyboard,linuxinput,lirc,ps2mouse,serialmouse,lirc   --prefix=/opt/opendfb  --with-tests  #--enable-debug;
+make  -j;sudo make install
